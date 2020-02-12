ec39b44cecd37dfaf33aad520a1450ddf6548f68
Nikita <56094872+playstation7@users.noreply.github.com>
2020-02-12 17:33:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,16 +54,7 @@ def run():
     while True:
       
       
-        if len(ball.lives) < 1:
-            
-            print_counter(screen,'You lose',(500,270))
-            ball.mov_x_f = 0 
-            ball.mox_y_f = 0
         
-        else:
-            ball.update(blocks,background,background_rect,screen,settings)
-            ball.blit()
-            print_counter(screen,settings.score,(500,270))  #print number score
         
         res = check_events(platform,screen,background,background_rect,settings.score,settings.level,settings.screen_width,settings.screen_height,settings)
         if res == True:  #if res True,that start new game
@@ -73,7 +64,16 @@ def run():
         screen.blit(background,background_rect)
         
         
-          
+        if len(ball.lives) < 1:
+            
+            print_counter(screen,'You lose',(500,270))
+            ball.mov_x_f = 0 
+            ball.mox_y_f = 0
+        
+        else:
+            ball.update(blocks,background,background_rect,screen,settings)
+            ball.blit()
+            print_counter(screen,settings.score,(500,270))  #print number score 
         
         blocks.draw(screen)
         
